a916fff86ddab2ead0196f771af532c61a1fd4b2
Samuel Patmore <daydreamapplications@googlemail.com>
2025-08-11 09:38:53 +0100

    Switch between podcast & episode lists

--- composeApp/src/commonMain/kotlin/com/dda/castyway/App.kt
+++ composeApp/src/commonMain/kotlin/com/dda/castyway/App.kt
@@ -2,24 +2,35 @@ package com.dda.castyway
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.dda.castyway.shared.repositories.PodcastRepository
+import com.dda.castyway.shared.repositories.Podcast
 import com.dda.castyway.shared.repositories.PodcastRepositoryImpl
 import com.dda.castyway.ui.PodcastScreen
 import com.dda.castyway.ui.PodcastViewModel
 import com.dda.castyway.ui.search.SearchPresenter
-import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun App(platformContext: PlatformContext) {
     MaterialTheme {
-        val podcastViewModel: PodcastViewModel = viewModel { PodcastViewModel() }
-        PodcastScreen(podcastViewModel)
 
-        val podcastRepository = remember { PodcastRepositoryImpl() }
-        val downloader = remember { Downloader(platformContext) }
-        val presenter = remember { SearchPresenter(podcastRepository = podcastRepository, downloader) }
-        SearchScreen(presenter)
+        var selectedPodcast by remember { mutableStateOf<Podcast?>(null) }
+
+        when (val podcast = selectedPodcast) {
+            null -> {
+                val podcastRepository = remember { PodcastRepositoryImpl() }
+                val downloader = remember { Downloader(platformContext) }
+                val presenter = remember { SearchPresenter(podcastRepository = podcastRepository, downloader) }
+                SearchScreen(presenter, onPodcastSelected = { selectedPodcast = it })
+            }
+
+            else -> {
+                val podcastViewModel: PodcastViewModel = viewModel { PodcastViewModel() }
+                PodcastScreen(podcastViewModel, podcast)
+            }
+        }
     }
 }
--- composeApp/src/commonMain/kotlin/com/dda/castyway/ui/PodcastScreen.kt
+++ composeApp/src/commonMain/kotlin/com/dda/castyway/ui/PodcastScreen.kt
@@ -1,9 +1,21 @@
 package com.dda.castyway.ui
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -11,9 +23,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.dda.castyway.shared.models.PodcastEpisode
+import com.dda.castyway.shared.repositories.Podcast
 
 @Composable
-fun PodcastScreen(viewModel: PodcastViewModel) {
+fun PodcastScreen(
+    viewModel: PodcastViewModel,
+    podcast: Podcast,
+) {
     val uiState by viewModel.uiState.collectAsState()
 
     // Load the podcast feed when the screen is first composed
@@ -21,7 +37,7 @@ fun PodcastScreen(viewModel: PodcastViewModel) {
     // and avoid reloading on every recomposition.
     // For this example, we'll load it once.
     // A sample RSS feed url is used here.
-    viewModel.loadPodcastFeed("https://feed.podbean.com/trashfuturepodcast/feed.xml")
+    viewModel.loadPodcastFeed(url = podcast.channelUrl)
 
     Surface(modifier = Modifier.fillMaxSize()) {
         when (val state = uiState) {
@@ -30,9 +46,11 @@ fun PodcastScreen(viewModel: PodcastViewModel) {
                     CircularProgressIndicator()
                 }
             }
+
             is PodcastUiState.Success -> {
                 PodcastList(state.feed.episodes)
             }
+
             is PodcastUiState.Error -> {
                 Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
                     Text(text = "Error: ${state.message}", color = MaterialTheme.colorScheme.error)
